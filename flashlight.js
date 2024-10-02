7d5c892e600888a2ad3a926a4068ac124d2c12e2
old-turtle <prymar@protonmail.com>
2024-10-01 20:59:27 -0400

    Adding Flashlight feature

---
+++ flashlight.js
@@ -0,0 +1,10 @@
+function toggleBackground() {
+    const body = document.body;
+    if (body.style.backgroundColor === 'white') {
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+    } else {
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+    }
+}
